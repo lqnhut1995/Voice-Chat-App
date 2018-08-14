ad34d5be8ed050aeae0bad74de5f439bdb433954
Hell Rocky <hellrocky@hells-imac.local>
2018-08-14 22:06:54 +0700

    Initial Commit

---
+++ Voice Chat App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voice Chat App
+//
+//  Created by Hell Rocky on 8/14/18.
+//  Copyright © 2018 Hell Rocky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
